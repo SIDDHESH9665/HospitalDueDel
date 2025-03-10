cca64e42a36779adc6dedacd4159d9cff13b8d63
Siddhesh Bhalerao <156121006+SIDDHESH9665@users.noreply.github.com>
2025-03-10 23:28:24 +0530

    Update FinancialAssessment.jsx

--- frontend/src/components/financialAss/FinancialAssessment.jsx
+++ frontend/src/components/financialAss/FinancialAssessment.jsx
@@ -9,30 +9,28 @@ const FinancialAssessment = ({ data }) => {
 
   return (
     <div className="financial-container">
-      <h3 style={{ marginBottom: "15px", fontSize: "30px" }}>Financial Assessment</h3>
+      <h3>Financial Assessment</h3>
+      <p>Assess the financial status of the hospital</p>
       <div className="financial-row">
         {["gst_status", "pan_status", "epfo_status"].map((key) => (
           <div className="financial-item" key={key}>
-            <img
-              src={logos[key]}
-              alt={key}
-              className="financial-logo"
-            />
-            <p>
-              {key.replace("_", " ").toUpperCase()}:{" "}
-              <span
-                style={{
-                  color:
-                    data?.[key]?.toLowerCase() === "valid"
-                      ? "green"
-                      : data?.[key]?.toLowerCase() === "invalid"
-                      ? "red"
-                      : "black",
-                }}
-              >
-                {data?.[key] || "Not Available"}
-              </span>
-            </p>
+            <img src={logos[key]} alt={key} className="financial-logo" />
+            <div style={{ color: "black" }}>
+              <div>{key.replace("_", " ").toUpperCase()}</div>
+                <div
+                  style={{
+                    color:
+                      data?.[key]?.toLowerCase() === "valid"
+                        ? "green"
+                        : data?.[key]?.toLowerCase() === "invalid"
+                        ? "red"
+                        : "orange",
+                    textAlign: "center",
+                  }}
+                >
+                  {data?.[key] || "Not Available"}
+                </div>
+            </div>
           </div>
         ))}
       </div>
